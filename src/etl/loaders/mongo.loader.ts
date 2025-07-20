56e403fa9226c118455e78143732dc1879a78afd
sebastianthomas94 <53168297+sebastianthomas94@users.noreply.github.com>
2025-07-20 23:50:40 +0530

    feat: index filter upsert

--- src/etl/loaders/mongo.loader.ts
+++ src/etl/loaders/mongo.loader.ts
@@ -6,17 +6,24 @@ export class MongoLoader extends Transform {
     private buffer: AnyBulkWriteOperation<any>[] = [];
 
     constructor(
-        private coll: Collection,
-        private batchSize = 500,
+        private readonly coll: Collection,
+        private readonly batchSize = 500,
+        private readonly uniqueFields: string[] = ["_id"],
     ) {
         super({ objectMode: true });
     }
 
     //eslint-disable-next-line @typescript-eslint/no-explicit-any
     async _transform(doc: any, _: BufferEncoding, done: TransformCallback) {
+        const filter: Record<string, unknown> = {};
+        for (const field of this.uniqueFields) {
+            filter[field] = doc[field];
+        }
+
         this.buffer.push({
-            updateOne: { filter: { _id: doc._id }, update: { $set: doc }, upsert: true },
+            updateOne: { filter, update: { $set: doc }, upsert: true },
         });
+
         if (this.buffer.length >= this.batchSize) {
             await this.coll.bulkWrite(this.buffer);
             this.buffer = [];
