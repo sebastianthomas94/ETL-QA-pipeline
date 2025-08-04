5c8f3bad2715e51d0064095ccedbe58437f253d5
sebastianthomas94 <sebastianthomas94@users.noreply.github.com>
2025-08-04 18:02:13 +0000

    chore: apply formatting changes

--- src/main.ts
+++ src/main.ts
@@ -42,10 +42,6 @@ async function bootstrap() {
 }
 void bootstrap();
 
-
 console.log(`Environment: ${process.env.NODE_ENV}`);
-
-
-
 
 console.log(`Server running on port: ${process.env.PORT ?? 3000}`);
