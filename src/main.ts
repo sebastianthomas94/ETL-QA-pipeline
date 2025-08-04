b8cb32ca0301659aec3f4ce7b6529841c9ae9fe9
sebastianthomas94 <53168297+sebastianthomas94@users.noreply.github.com>
2025-08-04 21:59:27 +0530

    no verify

--- src/main.ts
+++ src/main.ts
@@ -41,3 +41,12 @@ async function bootstrap() {
     await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
+
+console.log(`Environment: ${process.env.NODE_ENV}`);
+
+
+
+
+
+
+console.log(`Server running on: http://localhost:${process.env.PORT ?? 3000}/docs`);
