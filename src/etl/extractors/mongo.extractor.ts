90d71c0ee6252a1688a97d124667752c81d59eba
sebastianthomas94 <53168297+sebastianthomas94@users.noreply.github.com>
2025-11-18 11:18:47 +0530

    chore: expand MongoExtractor to handle missing updatedAt

--- src/etl/extractors/mongo.extractor.ts
+++ src/etl/extractors/mongo.extractor.ts
@@ -6,6 +6,8 @@ export class MongoExtractor {
     async streamCollection(collectionName: string, since: Date): Promise<FindCursor> {
         const client = await MongoClient.connect(this.uri);
         const coll = client.db().collection(collectionName);
-        return coll.find({ updatedAt: { $gte: since } });
+        return coll.find({
+            $or: [{ updatedAt: { $gte: since } }, { updatedAt: { $exists: false } }, { updatedAt: null }],
+        });
     }
 }
